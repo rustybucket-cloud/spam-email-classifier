d1532083c05ad55a987b4a488e63e7969619bac9
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2023-02-10 19:37:23 -0700

    save classifier function

--- train.py
+++ train.py
@@ -1,6 +1,24 @@
 import torch
 from preprocess import get_sets, get_vectors, get_tokens
 from model import Model, train, test
+import pickle
+
+class SpamModel:
+    def __init__(self, model, vectorizer, get_tokens):
+        self.model = model
+        self.vectorizer = vectorizer
+        self.get_tokens = get_tokens
+
+    def classify(self, document):
+        X = self.get_tokens([document])
+        X = self.vectorizer.transform(X)
+        with torch.no_grad():
+            pred = self.model.forward(torch.Tensor(X.toarray()))
+            if pred[0][0] > pred[0][1]:
+                return "Not Spam"
+            else:
+                return "Spam"
+    
 
 def main():
     X_train, X_test, y_train, y_test = get_sets()
@@ -19,6 +37,12 @@ def main():
     y_test = torch.Tensor(y_test)
     print(test(model, X_test, y_test))
     torch.save(model.state_dict(), f"./model.pt")
+
+    predictor = SpamModel(model, vectorizer, get_tokens)
+    
+    with open("./model.pkl", "wb") as f:
+        pickle.dump(predictor, f)
+
 
 if __name__ == "__main__":
     main()
